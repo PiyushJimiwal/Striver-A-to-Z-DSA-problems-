7d4b92ef880cb2082263ae088e48b8e5e920ad31
Piyush Singh Jimiwal <piyushjimiwal05@gmail.com>
2025-10-29 11:49:33 +0530

    algorithm of bubble sort

--- Sorting techique/bubble sort.cpp
+++ Sorting techique/bubble sort.cpp
@@ -1,1 +1,31 @@
+/*
+Let the array be arr[] of size n.
 
+Start from the first element and compare it with the next one.
+
+If arr[j] > arr[j+1], swap them.
+
+Move to the next pair and continue until the end of the array.
+→ After the first pass, the largest element will be at the end.
+
+Repeat the process for the remaining unsorted part (n-1, n-2, …).
+
+If during a pass no swaps are made, the array is already sorted — stop early. 
+
+*/
+
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            bool swaped = false;
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j] > nums[j+1])
+                    swap(nums[j], nums[j+1]);
+                    swaped = true;
+            }
+        }
+        return nums;
+    }
+};
