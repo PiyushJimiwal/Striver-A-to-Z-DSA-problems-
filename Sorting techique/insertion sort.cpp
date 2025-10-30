b9e0b88183211a90f7a4d28ab4f43d8a1b2725ed
Piyush Singh Jimiwal <piyushjimiwal05@gmail.com>
2025-10-30 11:06:52 +0530

    Algorithm of insertion osrt

--- Sorting techique/insertion sort.cpp
+++ Sorting techique/insertion sort.cpp
@@ -1,1 +1,26 @@
+/* Start from the second element (since a single element is trivially sorted).
+
+Compare the current element (called key) with the elements before it.
+
+Shift all elements greater than the key one position ahead.
+
+Insert the key into its correct position. */ 
+
+class Solution {
+public:
+    vector<int> insertionSort(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 1; i<n;i++){
+            int key = nums[i];
+            int j = i-1;
+
+            while(j>=0 && (nums[j] > key)){
+                nums[j+1] = nums[j];
+                j = j - 1;
+            }
+        nums[j+1] = key;
+        }
+        return nums;
+    }
+};
 
