63db032bf748b963671376b505c20a8d96941413
Piyush Singh Jimiwal <piyushjimiwal05@gmail.com>
2025-11-03 16:52:26 +0530

    Update Finding largest element in arrayt.cpp

--- Problem related to array/Easy/Finding largest element in arrayt.cpp
+++ Problem related to array/Easy/Finding largest element in arrayt.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int currentmax = nums[0];
+        int maxelement = nums[0];
+        int n = nums.size();
+        for(int i = 1 ; i<n; i++){
+            if(nums[i] >= currentmax){
+                currentmax = nums[i];
+                maxelement = currentmax;
+            }
+        }
+        return maxelement;
+    }
+};
