364bd09b8bed1415355195db505307705b3a2990
Piyush Singh Jimiwal <piyushjimiwal05@gmail.com>
2025-11-05 12:28:16 +0530

    Update Second largest element in an array without sorting.cpp

--- Problem related to array/Easy/Second largest element in an array without sorting.cpp
+++ Problem related to array/Easy/Second largest element in an array without sorting.cpp
@@ -1,1 +1,29 @@
+/*🔹 Algorithm (one step):
+Traverse the array once, keeping track of the largest and second largest elements —
+if the current element is greater than the largest, update second = largest and largest = current;
+else if it’s smaller than largest but greater than second, update second = current.
 
+✅ Time Complexity: O(n)
+✅ Space Complexity: O(1)
+*/
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+        //your code goes here
+        int second = INT_MIN;
+        int currentmax = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > currentmax){
+                second = currentmax;
+                currentmax = nums[i];
+            }
+            else if(nums[i] < currentmax && nums[i] > second){
+                second = nums[i];
+            }
+        }
+        if(secodn = INT_MIN){
+            return -1;
+        }
+        return second;
+    }
+};
