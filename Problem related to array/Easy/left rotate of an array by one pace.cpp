21b86b75f718b903628ea8ff18f4333add8bce6e
Piyush Singh Jimiwal <piyushjimiwal05@gmail.com>
2025-11-06 20:01:32 +0530

    Update left rotate of an array by one pace.cpp

--- Problem related to array/Easy/left rotate of an array by one pace.cpp
+++ Problem related to array/Easy/left rotate of an array by one pace.cpp
@@ -3,16 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
        int n = nums.size();
         k = k%n;
-        for(int i = 0; i<k; i++){
-            int last = nums[n-1];
-        
-        for(int j = n-1; j>0 ; j--){
-            nums[j] = nums[j-1];
-        }
-        nums[0] = last;
-    }
-        for(int x : nums){
-            cout<<x<<" ";
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
