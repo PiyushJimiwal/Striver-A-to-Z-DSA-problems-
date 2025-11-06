b722bc44df1307399e97b4676db2d5af3db29db9
Piyush Singh Jimiwal <piyushjimiwal05@gmail.com>
2025-11-06 21:21:47 +0530

    Add comments to moveZeroes function
    Added comments to explain the two-pointer approach for moving zeros to the end of the array.

--- Problem related to array/Easy/move Zero to end.cpp
+++ Problem related to array/Easy/move Zero to end.cpp
@@ -1,1 +1,17 @@
-
+/*first use the two pointer pointing to the first element of array
+now i is for scaning of the non zero element 
+j is for swaping the non zero element
+if non zero elment found then swap it with i and incrase j++*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0;
+        for(int i = 0; i<n ; i++){
+            if(nums[i] != 0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
